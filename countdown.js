29294253154d9d835abc4e93ec1550e990fbf2a7
mariosdaskalas <25797700+mariosdaskalas@users.noreply.github.com>
2021-12-13 15:01:40 +0200

    Add  to h,m,s

--- countdown.js
+++ countdown.js
@@ -21,10 +21,6 @@ let getHours = parseInt(document.getElementById("gethours").value);
 let getMinutes = parseInt(document.getElementById("getminutes").value);
 let getSeconds = parseInt(document.getElementById("getseconds").value);
 
-console.log(`Hours: ${getHours}`);
-console.log(`Minutes: ${getMinutes}`);
-console.log(`Seconds: ${getSeconds}`);
-
 /* Select  timer*/
 let stopwatch = document.getElementById("timer");
 
@@ -57,47 +53,36 @@ function setTimer() {
 function cycleTimer() {
     if (stop == false) {
 
+        // Start counting miliseconds
+        getSeconds--;
+    
+        /* When target is reached increment values */
+        if (getHours == 0 && getMinutes == 0 && getSeconds == 0) {
+            let audio = new Audio("./music/piano.mp3");
+            audio.play();
+            setTimeout(function(){
+                alert("Time is Over! Artist: Alexander Blu // Song: Soft Piano Song");
+            }, 500);
+            console.log(`Seconds1: ${getSeconds}`);
+            // window.location.reload();
+        }
         // Parse to Int
-        /*
         getHours = parseInt(getHours);
         getMinutes = parseInt(getMinutes);
         getSeconds = parseInt(getSeconds);
-        */
 
-        // Start counting miliseconds
-        getSeconds--;
-
-        /*
-        <audio id="myAudio">
-            <source src="/music/piano.mp3" type="audio/mpeg">
-            Your browser does not support this type of audio.
-            </source>
-        </audio>
-        */
-
-        
-        
-        /* When target is reached increment values */
-        if (getHours == 0 && getMinutes == 0 && getSeconds == 0) {
-            let audio = new Audio("/music/piano.mp3");
-            audio.play();
-            setTimeout(function(){
-                alert("Time is Over! Artist: Alexander Blu // Song: Soft Piano Song")
-            }, 500);
-            timer.innerHTML = "00:00:00";
-            // location.reload();
-        }
         if (getMinutes && getSeconds == 0) {
             getMinutes--;
-            getSeconds = 59;
+            getSeconds = 60;
         }
-        /*
-        if (getMinutes < 10) {
+        if (getSeconds < 10 || getSeconds == 0) {
+            getSeconds = "0" + getSeconds;
+        }
+        if (getMinutes < 10 || getMinutes == 0) {
             getMinutes = "0" + getMinutes;
         }
-        */
-        if (getSeconds < 10) {
-            getSeconds = "0" + getSeconds;
+        if (getHours < 10 || getHours == 0) {
+            getHours = "0" + getHours;
         }
     
         // Display data
@@ -113,5 +98,10 @@ function resetTimer() {
     getHours = 0;
     getMinutes = 0;
     getSeconds = 0;
+
+    getHours = parseInt(getHours);
+    getMinutes = parseInt(getMinutes);
+    getSeconds = parseInt(getSeconds);
+
     stopwatch.innerHTML = getHours + ":" + getMinutes + ":" + getSeconds;
 }
